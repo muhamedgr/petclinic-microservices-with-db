87d34eec1bc70ea4f12c7338ff0cf810c3acab2b
EC2 Default User <grmuhamed@gmail.com>
2026-03-29 16:28:01 +0000

    maintf-change

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -106,7 +106,7 @@ resource "aws_instance" "worker-1" {
   instance_type = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
-  subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
+  subnet_id = "subnet-0701c0f893aef8fdb"  # select own subnet_id of us-east-1a
   availability_zone = "us-east-1a"
   tags = {
     Name = "worker-1"
@@ -122,7 +122,7 @@ resource "aws_instance" "worker-2" {
   instance_type = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
-  subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
+  subnet_id = "subnet-0701c0f893aef8fdb"  # select own subnet_id of us-east-1a
   availability_zone = "us-east-1a"
   tags = {
     Name = "worker-2"
